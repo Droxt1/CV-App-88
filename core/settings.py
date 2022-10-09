d95d363ec2ec0a6373703d75bc6c44fa689295f9
Ahmed Amer Abd AL-Azeez <ahmed.amer56@mail.ru>
2022-10-09 13:37:13 +0300

    deleting packages

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -143,18 +143,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'cv.User'
 
 
-# jazzmen dark theme
-JAZZMIN_SETTINGS = {
-    # title of the window
-    'site_title': 'CV APP',
-
-    # Title on the login screen
-    'site_header': 'CV APP',
-
-    # Welcome text on the login screen
-    'welcome_sign': 'Welcome to CV APP',
-
-    # Title on the brand, and the login screen (19 chars max)
-    'site_title_short': 'CV APP',
-
-}
+# # jazzmen dark theme
+# JAZZMIN_SETTINGS = {
+#     # title of the window
+#     'site_title': 'CV APP',
+#
+#     # Title on the login screen
+#     'site_header': 'CV APP',
+#
+#     # Welcome text on the login screen
+#     'welcome_sign': 'Welcome to CV APP',
+#
+#     # Title on the brand, and the login screen (19 chars max)
+#     'site_title_short': 'CV APP',
+#
+# }
